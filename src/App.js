5d076b24d1392b56a55721e179090ca7ef052793
Franco Castany <francocastany@gmail.com>
2022-08-23 20:32:36 -0300

    Corrigiendo App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-/*
-import logo from './logo.svg';
-import './App.css';
+
+import NavBarHeader from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBarHeader/>
     </div>
-  );
+  )
 }
-
 export default App;
-
-*/
-import NavBarHeader from './components/NavBar';
-
-export default NavBarHeader;
 
