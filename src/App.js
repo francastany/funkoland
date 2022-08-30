2d598de6bed5c4fa3d018ea8f3a58cb883caa3b6
Franco Castany <francocastany@gmail.com>
2022-08-30 19:03:10 -0300

    Agregando Item List Container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import NavBarHeader from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       <NavBarHeader/>
+      <ItemListContainer 
+        greetings="Próximamente aquí estará el contenido =D"
+      />
     </div>
   )
 }
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+function ItemListContainer({greetings}){
+    return (
+        <>
+        <div className='container-fluid text-center mt-3'>
+            <p>{greetings}</p>
+        </div>
+        </>
+    );
+}
+
+export default ItemListContainer;
