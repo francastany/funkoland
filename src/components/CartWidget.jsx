5cabb1155a4d96fe61f994c41f9b04eaac5462a4
Franco Castany <francocastany@gmail.com>
2022-08-31 11:38:56 -0300

    Desafío CartWidget

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function CartWidget ({badgeContent}) {
+    return(
+    <button type="button" class="btn position-relative">
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
+            <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
+        </svg>
+        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-dark">
+            {badgeContent}
+            <span class="visually-hidden">cart items</span>
+        </span>
+    </button>    
+    )
+}
+
+export default CartWidget;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,8 @@
 import './navBar.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import CartWidget from './CartWidget';
+
 
 function NavBarHeader() {
   return (
@@ -14,11 +16,9 @@ function NavBarHeader() {
             </svg>
           </button>
           <span className="navbar-brand mb-0 h1">funkoLand</span>
-          <button>
-            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-bag" viewBox="0 0 16 16">
-              <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
-            </svg>
-          </button>
+          <CartWidget
+            badgeContent={5}
+          />
         </div>
       </nav>
       <nav className="categories container-fluid d-flex justify-content-evenly">
